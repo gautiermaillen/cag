994da3acd9b1b8dc7158fa26935bfd3cfe4abba1
internet09 <internet09>
2016-06-03 15:20:55 +0200

    Ajout T-shirt : Formulaire (+ Description)

--- v/js/scriptAjoutTshirt.js
+++ v/js/scriptAjoutTshirt.js
@@ -11,12 +11,13 @@ $(function(){
 		$ajout_nom = $("form.ajouterLTshirt input[name=nom]").val();
 		$ajout_prix = $("form.ajouterLTshirt input[name=prix]").val();
 		$ajout_date = $("form.ajouterLTshirt input[name=date]").val();
+		$ajout_desc = $("form.ajouterLTshirt textarea[name=desc]").val();
 		$ajout_img_gd = $("form.ajouterLTshirt input[name=img_gd]").val();
 		$ajout_img_pt = $("form.ajouterLTshirt input[name=img_pt]").val();
 		$ajout_createur = $("form.ajouterLTshirt select[name=createur] option:selected").text();
 		$ajout_matiere = $("form.ajouterLTshirt select[name=matiere] option:selected").text();
 		$ajout_categorie = $("form.ajouterLTshirt select[name=categorie] option:selected").text();
-
+		
 		$.getJSON(
 			"dispatcher.php",
 			{
@@ -24,6 +25,7 @@ $(function(){
 				nom : 		$ajout_nom,
 				prix : 		$ajout_prix,
 				date : 		$ajout_date,
+				desc : 		$ajout_desc,
 				img_gd : 	$ajout_img_gd,
 				img_pt : 	$ajout_img_pt,
 				createur: 	$ajout_createur,
