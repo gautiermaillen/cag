de683f23f3d587cddd26cad5f9ead92e537fbe6a
internet09 <internet09>
2016-06-06 14:46:21 +0200

    JQUERY : scriptVoirTshirt.js, fonctionnalité corrigé.

--- v/js/scriptVoirTshirt.js
+++ v/js/scriptVoirTshirt.js
@@ -1,21 +1,23 @@
 $(function(){
-	$("section#tshirt ul.lTshirt").on("click","li",afficheDetail);
+	$("section#tshirt ul.lTshirt").on("click",">li",afficheDetail);
 
-	function afficheDetail(){
-		// console.log(this);
-		// $tdata = {prod_nom:"Boule",prod_prix:"25€",prod_date:"25-06-15",prod_desc:"Français",cre_nom:"Moi",mat_nom:"coton",cat_nom:"illustré"};
-		// formulaire($tdata,this);
+	function afficheDetail(ev){
 		$leLI = this;
-		$.getJSON(
-			"dispatcher.php",
-			{
-				operation : "affichage",
-				id : 		$(this).attr("data-id")
-			},
-			function(data){
-				// console.log(data);
-				formulaire(data,$leLI);
-			}
-		);
+		if (!$($leLI).has("form").length){
+			$.getJSON(
+				"dispatcher.php",
+				{
+					operation : "affichage",
+					id : 		$(this).attr("data-id")
+				},
+				function(data){
+					// Voir dans fonctionUtile.js
+					formulaire(data[0],$leLI);
+				}
+			);
+		}
+		else{
+			$($leLI).children('form').remove();
+		}
 	}
 });
