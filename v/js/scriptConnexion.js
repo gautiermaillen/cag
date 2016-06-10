c5d73e65fab3169edfdfd17f784912a874244288
internet09 <internet09>
2016-06-10 10:39:41 +0200

    les accolades

--- v/js/scriptConnexion.js
+++ v/js/scriptConnexion.js
@@ -3,18 +3,19 @@ $(function(){
 
 	function verifOk(){
 		$.ajax(
-			"dispatcher.php",
-			method:"POST",
-			data:{
-				operation : "inscription",
-				login : 	$("input[name=login]"),
-				mdp : 		$("input[name=mdp")
-			},
-			success:function(doc,status,xhr){
-				alert(data);
-			},
-			error:function(){
-				alert("Erreur AJAX verifOk");
+			"dispatcher.php",{
+				method:"POST",
+				data:{
+					operation : "inscription",
+					login : 	$("input[name=login]"),
+					mdp : 		$("input[name=mdp")
+				},
+				success:function(doc,status,xhr){
+					alert(data);
+				},
+				error:function(){
+					alert("Erreur AJAX verifOk");
+				}
 			}
 		);
 	}
