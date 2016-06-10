16b9a2399968807ff9bcf6284fbf1ee464dc5f09
internet09 <internet09>
2016-06-10 10:27:30 +0200

    Ajout de scriptConnexion.js

--- v/js/scriptConnexion.js
+++ v/js/scriptConnexion.js
@@ -1,3 +1,17 @@
 $(function(){
-	
+	$("input[type=text]").on("keyup",verifOk);
+
+	function verifOk(){
+		$.ajax(
+			"dispatcher.php",
+			{
+				operation : "inscription",
+				login : 	$("input[name=login]"),
+				mdp : 		$("input[name=mdp")
+			},
+			function(data){
+				alert(data);
+			}
+		);
+	}
 });
