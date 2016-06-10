1297074ab60943c90a2b80e0816cbd5219ae3b36
internet05 <internet05>
2016-06-10 09:16:32 +0200

    Correction Connexion Adapter

--- m/ConnexionAdapter.php
+++ m/ConnexionAdapter.php
@@ -10,7 +10,7 @@
 			$this->pdo = $pdo;
 		}
         
-        public function verifierIdentite(){
+        public function verifierIdentite($login){
             /* sélectionner tous les users et les logins de la table administrateurs et vérifier s'il y en a un qui correspond au login qu'on fait passer*/
             $sql = "
             SELECT
@@ -18,9 +18,10 @@
                 admin_login,
                 admin_pwd
 				FROM administrateurs
+                WHERE admin_login=:a
             ";
 			$stmt = $this->pdo->prepare($sql);
-			$stmt->execute();
+			$stmt->execute([":a"=>$login]);
 			$this->verifierIdentite = $stmt->fetchAll(PDO::FETCH_CLASS, "Tshirt");
 			return $this->verifierIdentite;
         }
