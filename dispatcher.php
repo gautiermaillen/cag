554f36c1525df5b9c7b992411d747b3669a23345
Gautier Maillen <gautier.maillen@gmail.com>
2016-06-13 13:47:50 +0200

    Revert "Revert "Dispatcher à ignorer""
    This reverts commit 263736fcfc659c9bfa01eebf251473eb163bce46.

--- dispatcher.php
+++ dispatcher.php
@@ -23,12 +23,6 @@
 		$op = "liste"; // par défaut
 	}
 
-	// $op = (isset($_POST['operation']))?$_POST['operation']:1;
-	// if(!isset($operation_permise[$op]))
-	// {
-	// 	$op = "liste"; // par défaut
-	// }
-
 	$Tc = new TshirtControler();
 	$Cc = new ConnexionControler();
 
