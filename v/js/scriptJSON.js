ec2867997df32f9e94cf10d9c68266568c845355
internet09 <internet09>
2016-06-14 08:39:17 +0200

    Correction des icones

--- v/js/scriptJSON.js
+++ v/js/scriptJSON.js
@@ -26,7 +26,7 @@ $(function(){
 			boucleFor(data["tabCategories"],"option",$("form.ajouterLTshirt select[name=categorie]"),"cat_nom","cat_id");
 			
 			// "fonctionUtile.js", crée les icones sur chaque li
-			lesIcones($("section#tshirt ul.lTshirt li"));
+			setTimeout(lesIcones,50,$("section#tshirt ul.lTshirt li"));
 		}
 
 	);
@@ -49,6 +49,7 @@ $(function(){
 				boucleFor(data["tabCreateurs"],"option",$($selectCreateurs),"cre_nom","cre_id");
 				boucleFor(data["tabMatieres"],"option",$($selectMatieres),"mat_nom","mat_id");
 				boucleFor(data["tabCategories"],"option",$($selectCategories),"cat_nom","cat_id");
+				setTimeout(lesIcones,50,$("section#tshirt ul.lTshirt li"));
 			}
 		);
 	}
@@ -71,6 +72,7 @@ $(function(){
 				// fonction créée dans le fichier JS "fonctionUtile.js"
 				supprimerEnfant("section#tshirt ul.lTshirt");
 				boucleFor(data["tabNomsTshirt"],"li",$("section#tshirt ul.lTshirt"),"prod_nom","prod_id");
+				setTimeout(lesIcones,50,$("section#tshirt ul.lTshirt li"));
 			}
 		);
 	}
--- v/js/scriptMoteurRecherche.js
+++ v/js/scriptMoteurRecherche.js
@@ -14,7 +14,7 @@ $(function(){
 				supprimerEnfant("section#tshirt ul.lTshirt");
 				// fonction créée dans le fichier JS "fonctionUtile.js"
 				boucleFor(data["tabNomsTshirt"],"li",$("section#tshirt ul.lTshirt"),"prod_nom","prod_id");
-				setTimeout(lesIcones,500,$("section#tshirt ul.lTshirt li"));
+				setTimeout(lesIcones,50,$("section#tshirt ul.lTshirt li"));
 					// "fonctionUtile.js", crée les icones sur chaque li
 					//lesIcones($("section#tshirt ul.lTshirt li"));
 			}
